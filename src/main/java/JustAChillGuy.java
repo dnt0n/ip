e757d9889f26eff9c219697c855842bcdab9ddbe
Danton <dantonyap2016@gmail.com>
2025-08-22 00:12:12 +0800

    added echoing function

--- src/main/java/JustAChillGuy.java
+++ src/main/java/JustAChillGuy.java
@@ -1,12 +1,37 @@
+import java.util.Scanner;
+
 public class JustAChillGuy {
+    public static String wrapInLines(String str) {
+        String[] lines = str.split("\n");
+        StringBuilder sb = new StringBuilder();
+        sb.append("____________________________________________________________\n");
+        for (String line : lines) {
+            sb.append("  ").append(line).append("\n");
+        }
+        sb.append("____________________________________________________________\n");
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
+        boolean isRunning = true;
+        Scanner sc = new Scanner(System.in);
         String name = "Just A Chill Guy";
-        String greeting = "____________________________________________________________\n"
-                + "Yo! I am " + name + " :) \n"
-                + "How can I help ya?\n"
-                + "____________________________________________________________\n"
-                + "See you man!\n"
-                + "____________________________________________________________";
-        System.out.println(greeting);
+        String greetingMessage = "Yo! I am ✨" + name + "✨ :) \n" + "How can I help ya?";
+        String goodbyeMessage = "Goodbye mate! See ya next time!";
+
+        System.out.println(wrapInLines(greetingMessage));
+
+        while (isRunning) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                isRunning = false;
+                System.out.println(wrapInLines(goodbyeMessage));
+            } else {
+                System.out.println(wrapInLines(input));
+            }
+        }
+
+        sc.close();
     }
 }
