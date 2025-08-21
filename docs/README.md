363a5437cfdb1f60b1830689dbdc2a0715b187da
Danton <dantonyap2016@gmail.com>
2025-08-21 23:46:36 +0800

    renamed duke and implemented greet and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# JustAChillGuy User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/JustAChillGuy.java
@@ -0,0 +1,12 @@
+public class JustAChillGuy {
+    public static void main(String[] args) {
+        String name = "Just A Chill Guy";
+        String greeting = "____________________________________________________________\n"
+                + "Yo! I am " + name + " :) \n"
+                + "How can I help ya?\n"
+                + "____________________________________________________________\n"
+                + "See you man!\n"
+                + "____________________________________________________________";
+        System.out.println(greeting);
+    }
+}
