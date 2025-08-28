88692a562e48e4c328cbf9b836f7a0074ef04876
Danton <dantonyap2016@gmail.com>
2025-08-28 13:53:12 +0800

    Add enums for command types
    Add enums to command types for safer input handling and better command classification

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,5 +1,5 @@
 public enum Command {
-    BYE, HELLO, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, UNKNOWN;
+    HELLO, BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, UNKNOWN;
 
     public static Command from(String commandWord) {
         try {
